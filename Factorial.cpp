39ac0ff6d84da4515eeb1cb52547c7631fc1dbad
mihirp5027 <154262466+mihirp5027@users.noreply.github.com>
2023-12-20 16:04:07 +0530

    Add files via upload

---
+++ Factorial.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+    int n;
+    int f=1;
+    int c=0;
+    cin>>n;
+    c=+3;
+
+    for(int i =1; i<=n; i++)
+    {
+        f=f*i;
+        c=+3;
+
+
+    }
+    cout<<f<<endl;
+    cout<<c;
+}
+
